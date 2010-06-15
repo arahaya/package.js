8d85ada240423dffaf248451241f036c648c4ec8
ARAKI Hayato <arahaya.com@gmail.com>
2010-06-16 04:46:15 +0900

    first added

---
+++ README.md
@@ -0,0 +1,110 @@
+Package.js
+======
+
+A simple Javascript class library.
+Planned to be a part of [CanvasScript3](http://github.com/arahaya/CanvasScript3)
+
+
+## Class Example ##
+
+    Class('Cat', function()
+    {
+        //constructor
+        this.Cat = function(name)
+        {
+            this._name = name;
+        };
+        
+        //getter
+        this.__get__name = function()
+        {
+            return this._name;
+        };
+    });
+    
+    var cat = new Cat('Garfield');
+    alert(cat.name); // Garfield
+
+
+## Extends Example ##
+
+    Class('Animal', function()
+    {
+        //constructor
+        this.Animal = function(age)
+        {
+            this._age = age;
+        };
+        
+        //getter
+        this.__get__age = function()
+        {
+            return this._age;
+        };
+    });
+    
+    Class('Cat').Extends(Animal, function()
+    {
+        //constructor
+        this.Cat = function(name, age)
+        {
+            this.__super__.constructor(age);
+            this._name = name;
+        };
+        
+        //getter
+        this.__get__name = function()
+        {
+            return this._name;
+        };
+    });
+    var cat = new Cat('Garfield', 20);
+    alert(cat.name); // Garfield
+    alert(cat.age); // 20
+
+
+## Package Example ##
+
+    package("com.arahaya.js", function()
+    {
+        Class('Cat', function()
+        {
+            //constructor
+            this.Cat = function(name)
+            {
+                this._name = name;
+            };
+            
+            //getter
+            this.__get__name = function()
+            {
+                return this._name;
+            };
+        });
+    });
+    
+    var cat = new com.arahaya.js.Cat('Garfield');
+    alert(cat.name); // Garfield
+
+
+## To Support IE ##
+
+    Class('Cat', function()
+    {
+        //constructor
+        this.Cat = function(name)
+        {
+            this._name = name;
+        };
+        
+        //getter
+        this.__get__name = function()
+        {
+            return this._name;
+        };
+    });
+    
+    var cat = new Cat('Garfield');
+    //use a method for getters and setters
+    alert(cat.getName()); // Garfield
+
---
+++ package.js
@@ -0,0 +1,162 @@
+/*
+    CanvasScript3
+    Copyright (c) 2010 ARAKI Hayato
+    
+    MIT License
+    
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+    
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+*/
+var package;
+var Class;
+(function()
+{
+    var currentNamespace = window;
+    
+    package = function(namespace, packageCode)
+    {
+        if (arguments.length == 2) {
+            //namespace and packageCode was passed
+            var paths = namespace.split('.');
+            for (var i = 0, l = paths.length; i < l; ++i)
+            {
+                var path = paths[i];
+                if (currentNamespace[path] === undefined) {
+                    currentNamespace[path] = {};
+                }
+                currentNamespace = currentNamespace[path];
+            }
+        }
+        else if (arguments.length == 1) {
+            //only the packageCode was passed
+            //use the global namespace
+            packageCode = namespace;
+        }
+        else {
+            return;
+        }
+        
+        packageCode.call(currentNamespace);
+        currentNamespace = window;
+    };
+
+    Class = function(className, classObject)
+    {
+        extendClass(Object, className, classObject);
+        
+        return {
+            Extends: function(superClass, classObject)
+            {
+                extendClass(superClass, className, classObject);
+            }
+        };
+    };
+    
+    extendClass = function(superClass, className, classObject)
+    {
+        //normalize the classObject
+        if (classObject === undefined) {
+            classObject = {};
+        }
+        else if (typeof(classObject) === 'function') {
+            classObject = new classObject();
+        }
+        
+        if (classObject[className] === undefined) {
+            classObject[className] = function(){};
+        }
+        
+        //create the subClass constructor
+        var subClass = classObject[className];
+        
+        //extend the superClass
+        var superCopy = function(){};
+        superCopy.prototype = superClass.prototype;
+        subClass.prototype = new superCopy();
+        subClass.prototype.constructor = subClass;
+        
+        //add a reference to the superClass
+        //TODO: not sure if this is good enough
+        subClass.prototype.__super__ = superClass.prototype;
+        
+        //set the default toString methods
+        subClass.toString = function()
+        {
+            return '[class ' + className + ']';
+        }
+        
+        subClass.prototype.toString = function()
+        {
+            return '[object ' + className + ']';
+        }
+        
+        //copy the properties from the classObject
+        //to the subClass constructor
+        var preffix, accessorName, accessorMethod;
+        for (var property in classObject)
+        {
+            if (property == className) {
+                //do not copy the constructor
+                continue;
+            }
+            
+            preffix = property.substr(0, 7);
+            
+            //Getters
+            if (preffix == '__get__') {
+                //define getters
+                accessorName = property.substr(7);
+                if (subClass.prototype.__defineGetter__) {
+                    subClass.prototype.__defineGetter__(accessorName, classObject[property]);
+                }
+                
+                //also copy as a public method for IE
+                //eg: obj.__get__methodName will become obj.getMethodName()
+                accessorMethod = 'get' + accessorName.charAt(0).toUpperCase() + accessorName.slice(1);
+                if (!subClass.hasOwnProperty(accessorMethod)) {
+                    //do not override if a method or property with the same name exists
+                    subClass.prototype[accessorMethod] = classObject[property];
+                }
+            }
+            
+            //Setters
+            else if (preffix == '__set__') {
+                //define setters
+                accessorName = property.substr(7);
+                if (subClass.prototype.__defineSetter__) {
+                    subClass.prototype.__defineSetter__(accessorName, classObject[property]);
+                }
+                
+                //also copy as a public method for IE
+                //eg: obj.__set__methodName will become obj.setMethodName()
+                accessorMethod = 'set' + accessorName.charAt(0).toUpperCase() + accessorName.slice(1);
+                if (!subClass.hasOwnProperty(accessorMethod)) {
+                    //do not override if a method or property with the same name exists
+                    subClass.prototype[accessorMethod] = classObject[property];
+                }
+            }
+            
+            subClass.prototype[property] = classObject[property];
+        }
+        
+        currentNamespace[className] = subClass;
+    };
+})();
